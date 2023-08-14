b6ad102e5a1ff134bbc19b27f9fb24d85666ace1
Pheonix747hs <94136752+Pheonix747hs@users.noreply.github.com>
2023-08-14 23:06:24 +0530

    Initial commit

---
+++ WPF_PDFDocument/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_PDFDocument.Controls;
+
+namespace WPF_PDFDocument
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PdfViewer viewer = new PdfViewer();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                newn.PdfPath = fileDialog.FileName;
+            }
+        }
+    }
+}
