0a8a355d4b7ebfcdf0b728aeab1004e51739b6d1
Pheonix747hs <94136752+Pheonix747hs@users.noreply.github.com>
2023-08-15 14:57:42 +0530

    setup

---
+++ WPF_PDFDocument/Controls/img_to_pdf.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_PDFDocument.Controls
+{
+    /// <summary>
+    /// Interaction logic for img_to_pdf.xaml
+    /// </summary>
+    public partial class img_to_pdf : UserControl
+    {
+        public img_to_pdf()
+        {
+            InitializeComponent();
+        }
+    }
+}
